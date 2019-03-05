18a69522b166ef7d93cd694aeb0e5d3e23597d16
webfansplz <308241863@qq.com>
2019-03-05 16:18:05 +0800

    new

--- new/index.js
+++ new/index.js
@@ -7,7 +7,7 @@ function new2(func) {
   const o = Object.create(func.prototype);
   //获取要实例化的实参
   const args = [].slice.call(arguments, 1);
-  //执行构造函数,并用apply将this指向硬绑定到构造函数
+  //执行构造函数,并用apply将this指向硬绑定到实例上
   const res = func.apply(o, args);
   //如果是引用类型,返回该引用类型,否则返回实例
   return ['object', 'function'].includes(typeof res) ? res : o;
